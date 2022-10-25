9aecfb74ebe43dee3613ac13faa9ddcd0d916973
Denwiliusz27 <danwielgosz@gmail.com>
2022-10-26 00:15:04 +0200

    cw02

--- cw02/zad_03.py
+++ cw02/zad_03.py
@@ -3,18 +3,16 @@ import numpy
 
 
 def add_pic(img, key):
-    a_img = numpy.array(img, dtype=numpy.uint8)
-    a_key = numpy.array(key, dtype=numpy.uint8)
+    a_img = numpy.array(img, dtype=numpy.uint8)  # stworzenie tablicy typu uint8 z obrazka
+    a_key = numpy.array(key, dtype=numpy.uint8)  # stworzenie tablicy typu uint8 z obrazka
 
+    # tworzę tablicę o wymiarach takich jak
+    # obrazek typu uint8 i wypełniam zerami
     result = numpy.random.randint(1, size=(heigh, width), dtype=numpy.uint8)
 
     for i in range(0, len(a_img)):
         for j in range(0, len(a_img[0])):
-            a = a_img[i][j]
-            k = a_key[i][j]
-            if a == k:
-                result[i][j] = 0
-            else:
+            if a_img[i][j] != a_key[i][j]:  # jeśli bity na odpowiadających sobie pozycjach są różne, wstawiane jest 255
                 result[i][j] = 255
 
     return result
@@ -24,10 +22,12 @@ if __name__ == '__main__':
     miki = Image.open('miki.png').convert('L')
     quest = Image.open('quest.png').convert('L')
 
-    width, heigh = miki.size
+    width, heigh = miki.size  # pobranie wymiarów obrazka
 
+    # stworzenie klucza jako tablicy o podanych wymiarach z randomowo wstawionymi 0 lub 1
     key = numpy.random.randint(0, 2, size=(heigh, width), dtype=numpy.uint8)
 
+    # 1 w tablicy zamieniane na 255
     for i in range(0, heigh):
         for j in range(0, width):
             if key[i][j] == 1:
@@ -38,6 +38,8 @@ if __name__ == '__main__':
 
     miki_coded = add_pic(miki, key)
     miki_coded_img = Image.fromarray(miki_coded)
+
+    # zapisanie zakodowanego obrazku do pliku .png
     miki_coded_img.save("miki_coded.png", "PNG")
 
     quest_coded = add_pic(quest, key)
---
+++ cw02/zad_04.py
@@ -0,0 +1,97 @@
+def find_word(msg_sum, word):
+    word_b = [0] * len(word)
+
+    for i in range(0, len(word)):
+        word_b[i] = ord(word[i])
+
+    for i in range(0, len(msg_sum) - len(word_b)):
+        msg_with_key = msg_sum.copy()
+        j = 0
+
+        while j < len(word_b):
+            msg_with_key[i + j] = msg_sum[i + j] ^ word_b[j]
+            j += 1
+
+        for a in range(i, len(word_b) + i):
+            msg_with_key[a] = chr(msg_with_key[a])
+
+        print('Word on pos = ', i, ': ', msg_with_key[i:i + len(word_b)])
+
+
+def add_str(msg1, msg2):
+    msg_sum = [''] * len(msg1)
+    msg1_b = [0] * len(msg1)
+    msg2_b = [0] * len(msg2)
+    # msg_sum_b = [0] * len(msg1)
+
+    for i in range(0, len(msg1)):
+        msg1_b[i] = ord(msg1[i])
+        msg2_b[i] = ord(msg2[i])
+        msg_sum[i] = chr(msg1_b[i] ^ msg2_b[i])
+        # msg_sum_b[i] = msg1_b[i] ^ msg2_b[i]
+
+    return msg_sum
+
+
+def add_str_to_message_on_index(msg, index, str):
+    j = 0
+
+    for i in range(index, index + len(str)):
+        msg[i] = str[j]
+        j += 1
+
+    print("msg: ", msg)
+
+    return msg
+
+
+if __name__ == '__main__':
+    msg1_enc = '\x1e\x17\x0c\x12\x1b\x08\x0cf\x0e\x11x\x1a\x1c\x12o\x06\x18\x1f\x17\x03\x10\x01fkh\x1f\x08'
+    msg2_enc = '\x0b\ni\x07\x1c\x02k\x1f\x16e\x01\x0b\x07\x03\n\x15\x15\x0c\x1el\x07\x03\x03\x16\x01\x01\x02'
+
+    message_sum = add_str(msg1_enc, msg2_enc)
+    print(message_sum)
+
+    msg1_encoded = [''] * len(msg1_enc)
+    msg2_encoded = [''] * len(msg1_enc)
+
+    msg1_final = ''
+    msg2_final = ''
+
+    j = 0
+    while (1):
+        print('\n Enter possible word: ')
+        key = input()
+
+        if key == '-1':
+            break
+
+        for i in range(0, len(message_sum) - len(key) + 1):
+            encoded_str = add_str(message_sum[i:i + len(key)], key)
+            print('i = ', i, ': ', encoded_str)
+
+        print('\n Enter index nr: ')
+        index = int(input())
+
+        if index == -1:
+
+            break
+
+        encoded_str = add_str(message_sum[index:index + len(key)], key)
+        print('index = ', index, ': ', encoded_str)
+
+        if j % 2 == 0:
+            add_str_to_message_on_index(msg1_encoded, index, key)
+            add_str_to_message_on_index(msg2_encoded, index, encoded_str)
+        else:
+            add_str_to_message_on_index(msg1_encoded, index, encoded_str)
+            add_str_to_message_on_index(msg2_encoded, index, key)
+
+        j += 1
+
+    for n in range(0, len(msg1_encoded)):
+        msg1_final = msg1_final + msg1_encoded[n]
+        msg2_final = msg2_final + msg2_encoded[n]
+
+    print("Message 1: ", msg1_final)
+    print("Message 2: ", msg2_final)
